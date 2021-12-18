aff50628952afa378cc5a59f9aa6f3e6fe1fe603
chae young hoon <chaeyh04@gmail.com>
2021-12-19 08:04:45 +0900

    UPDATE * Ending

--- Assets/Script/UI/Fade.cs
+++ Assets/Script/UI/Fade.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Fade : MonoBehaviour
 {
@@ -13,6 +14,12 @@ public class Fade : MonoBehaviour
         action = () => { };
         Global.fade = this;
         anim.Play("Disappear");
+        if (Global.SceneCheck("Map") || Global.SceneCheck("Ending"))
+        {
+            Global.winSetting.btnMap.onClick.RemoveAllListeners();
+            Global.winSetting.btnMap.onClick.AddListener(() => { Global.SceneMove("Title"); });
+            Global.winSetting.btnMap.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Back To Title";
+        }
     }
 
     public void AppearSuccess()
