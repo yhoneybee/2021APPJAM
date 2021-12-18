22140c1f2678905d3e1e301d077efec8df9fcc90
chae young hoon <chaeyh04@gmail.com>
2021-12-19 04:07:08 +0900

    Coal Start

---
+++ Assets/Script/Coal/Coal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coal : Singletone<Coal>
+{
+    public List<Human> humans = new List<Human>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
